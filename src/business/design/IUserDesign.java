52a4b2108bf7172123191c120f83e8ad40587052
NguyenChien90 <daotao.ra@rikkeisoft.com>
2024-04-05 17:25:15 +0700

    up

--- src/business/design/IUserDesign.java
+++ src/business/design/IUserDesign.java
@@ -2,7 +2,7 @@ package business.design;
 
 import business.entity.User;
 
-public interface IUserDesign extends IGenericDesign<User>{
+public interface IUserDesign extends IGenericDesign<Integer>{
     void checkLogin();
     void logOut();
 }
--- src/business/designimpl/UserDesignImpl.java
+++ src/business/designimpl/UserDesignImpl.java
@@ -26,7 +26,7 @@ public class UserDesignImpl implements IUserDesign {
     }
 
     @Override
-    public Integer findIndexById(User id) {
+    public Integer findIndexById(Integer id) {
         return null;
     }
 
